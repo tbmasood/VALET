afd6c123091d8b66143b1f6e637158ba8b3b76f2
Talha Bin Masood <tbmasood1988@gmail.com>
2020-12-22 23:18:19 +0100

    Added setup.py file for package distribution

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="electrans",
+    version="0.0.1",
+    author="Talha Bin Masood",
+    author_email="talha.bin.maasood@liu.se",
+    description="A library for computing electronic charges and charge trasfer using NTOs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tbmasood/ElecTrans",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
