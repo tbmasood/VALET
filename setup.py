4712ec3eb061845c885ae2da49b863ae08e11b0c
Talha Bin Masood <talha.bin.masood@liu.se>
2023-07-05 12:26:48 +0200

    Now using toml file for setup

--- setup.py
+++ setup.py
@@ -1,26 +1,5 @@
-import setuptools
+# setup.py
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+from setuptools import setup
 
-setuptools.setup(
-    name="VALET",
-    version="0.0.1",
-    author="Talha Bin Masood",
-    author_email="talha.bin.masood@liu.se",
-    description="A library for computing electronic charges and charge transfer "
-        "in electronic transition using Natural Transition Orbitals.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tbmasood/VALET",
-    license="MIT",
-    platforms=["Unix", "MacOS", "Windows"],
-    install_requires=["numpy>=1.19.4", "matplotlib>=3.3.3", "cvxpy>=1.1.7"],
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+setup()
